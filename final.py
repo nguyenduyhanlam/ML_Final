c34110a5d86bab843dd2edeee5e12a986208a2d6
lam.nguyen <nguyenduyhanlam@yahoo.com.vn>
2020-05-11 18:09:09 +0700

    day1

---
+++ final.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 18:03:42 2020
+
+@author: lam.nguyen
+"""
+# Import necessary modules and set options
+import pandas as pd
+import numpy as np
+import itertools
+
+from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV, ElasticNetCV, LarsCV
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Load data
+data = pd.read_csv("prostate_data", sep = "\t")
+print(data.head())
+
+# Train-test split
+y_train = np.array(data[data.train == "T"]['lpsa'])
+y_test = np.array(data[data.train == "F"]['lpsa'])
+X_train = np.array(data[data.train == "T"].drop(['lpsa', 'train'], axis=1))
+X_test = np.array(data[data.train == "F"].drop(['lpsa', 'train'], axis=1))
